317c64e1dc64530ea5bf05dafd92caa856b70998
Richard Astbury <richard.astbury@gmail.com>
2016-02-01 16:31:05 +0000

    added nuspec

--- OrleansDashboard/Properties/AssemblyInfo.cs
+++ OrleansDashboard/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OrleansDashboard")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An admin dashboard for Microsoft Orleans")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("OrleansContrib")]
 [assembly: AssemblyProduct("OrleansDashboard")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
