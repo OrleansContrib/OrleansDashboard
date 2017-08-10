0cacc536941a654b66997529aa83e07a4b2b186b
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2017-08-11 01:15:17 +0300

    Support for message-based grains (uniform api)

---
+++ TestGrains/TestMessageBasedGrain.cs
@@ -0,0 +1,24 @@
+﻿using Orleans;
+using System;
+using System.Threading.Tasks;
+
+namespace TestGrains
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class MessageArgumentAttribute : Attribute
+    {}
+
+    public interface ITestMessageBasedGrain : IGrainWithIntegerKey
+    {
+        Task Receive(object message);
+    }
+
+    public class TestMessageBasedGrain : Grain, ITestMessageBasedGrain
+    {
+        [MessageArgument]
+        public Task Receive(object message)
+        {
+            return TaskDone.Done;
+        }
+    }
+}
