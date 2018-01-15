97f762257499f4eb68e67fa26689c2f439b2a419
Richard Astbury <richard.astbury@gmail.com>
2018-01-15 12:41:11 +0000

    don't report incorrect uptime. #42

--- OrleansDashboard/ISiloDetailsProvider.cs
+++ OrleansDashboard/ISiloDetailsProvider.cs
@@ -30,7 +30,7 @@ namespace OrleansDashboard
             // todo this could be improved by using a ISiloStatusListener
             // and caching / projecting the changes instead of polling
             // should reduce allocations of array's etc
-        
+
             return Task.FromResult(siloStatusOracle.GetApproximateSiloStatuses(true)
                 .Select(x => new SiloDetails()
                 {
