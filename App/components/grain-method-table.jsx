af6e6c445b84b32f5c85c5d489a491b0275b40cb
Richard Astbury <richard.astbury@gmail.com>
2018-06-26 13:05:33 +0100

    fix table overflow and header text margin

--- App/components/grain-method-table.jsx
+++ App/components/grain-method-table.jsx
@@ -3,16 +3,17 @@ var React = require('react');
 module.exports = React.createClass({
     renderRow:function(value){
         return <tr key={`${value.grain}.${value.method}`}>
-            <td style={{textOverflow: "ellipsis"}}>
+            <td style={{wordWrap:"break-word"}}>
+                <span className="pull-right"><strong>{this.props.valueFormatter(value)}</strong></span>
                 {value.method}
                 <br/><small><a href={`#/grain/${value.grain}`}>{value.grain}</a></small>
             </td>
-            <td style={{"textAlign":"right"}}><strong>{this.props.valueFormatter(value)}</strong></td>
+            
         </tr>
 
     },
     render:function(){
-        return <table className="table">
+        return <table className="table" style={{tableLayout:"fixed", width:"100%"}}>
             <tbody>
                 {(this.props.values || []).map(this.renderRow)}
                 {(this.props.values. length ? null : <tr><td><i>No data</i></td></tr>)}
